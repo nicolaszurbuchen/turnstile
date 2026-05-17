da8129924495e75099eebc9acb488d19508ed4e7
Nicolas Zurbuchen <nicolas.zurbuchen@outlook.com>
2026-05-17 12:43:12 +0900

    Fix App.

--- composeApp/src/commonMain/kotlin/io/nicolaszurbuchen/turnstile/App.kt
+++ composeApp/src/commonMain/kotlin/io/nicolaszurbuchen/turnstile/App.kt
@@ -1,13 +1,11 @@
 package io.nicolaszurbuchen.turnstile
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import io.nicolaszurbuchen.turnstile.core.design.theme.TurnstileTheme
 import io.nicolaszurbuchen.turnstile.core.navigation.NavGraph
 
 @Composable
-@Preview
-private fun AppPreview() {
+fun App() {
     TurnstileTheme {
         NavGraph()
     }
