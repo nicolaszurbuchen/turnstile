4800f5e6c902afc4962ee8da02af36866a483155
Nicolas Zurbuchen <nicolas.zurbuchen@outlook.com>
2026-05-20 22:33:48 +0900

    Removed desktop and web app.

--- shared/src/jsMain/kotlin/io/nicolaszurbuchen/turnstile/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package io.nicolaszurbuchen.turnstile
-
-class JsPlatform : Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
--- shared/src/wasmJsMain/kotlin/io/nicolaszurbuchen/turnstile/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package io.nicolaszurbuchen.turnstile
-
-class WasmPlatform : Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
